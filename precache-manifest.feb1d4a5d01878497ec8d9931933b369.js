672062fff1786060eaf002222e972c85e9d49b68
João Forja <joao.forja@challenge.pt>
2019-09-28 21:46:29 +0100

    Updates

---
+++ precache-manifest.feb1d4a5d01878497ec8d9931933b369.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1b0f3ed2d02588f753c0bb3a0a0db82",
+    "url": "/BarchartD3React/index.html"
+  },
+  {
+    "revision": "e1659a9d6eb879aa8789",
+    "url": "/BarchartD3React/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "4ba9bc86e2a035ebf2ce",
+    "url": "/BarchartD3React/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "e1659a9d6eb879aa8789",
+    "url": "/BarchartD3React/static/js/2.df2f4005.chunk.js"
+  },
+  {
+    "revision": "4ba9bc86e2a035ebf2ce",
+    "url": "/BarchartD3React/static/js/main.ba197f26.chunk.js"
+  },
+  {
+    "revision": "9ec92f791f9f75f0283d",
+    "url": "/BarchartD3React/static/js/runtime-main.393a971b.js"
+  }
+]);
